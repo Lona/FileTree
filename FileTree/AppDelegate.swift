197ea59d272888b93b93aad47e6e948e4a374f2e
Devin Abbott <devinabbott@gmail.com>
2018-08-17 16:10:19 -0700

    Initial Commit

---
+++ FileTree/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FileTree
+//
+//  Created by Devin Abbott on 8/17/18.
+//  Copyright © 2018 BitDisco, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
